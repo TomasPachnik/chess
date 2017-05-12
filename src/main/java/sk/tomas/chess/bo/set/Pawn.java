a27bbf8d71eb66e759e3a5a38133ed9e17cef2f3
Tomas Pachnik <tomas.pachnik@gmail.com>
2017-05-12 20:53:29 +0200

    added Pawn movement logic

--- src/main/java/sk/tomas/chess/bo/set/Pawn.java
+++ src/main/java/sk/tomas/chess/bo/set/Pawn.java
@@ -4,8 +4,10 @@ import sk.tomas.chess.bo.ChessBoard;
 import sk.tomas.chess.bo.Figure;
 import sk.tomas.chess.bo.Move;
 import sk.tomas.chess.bo.Position;
+import sk.tomas.chess.constants.Constants;
 
 import java.awt.*;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -19,7 +21,47 @@ public class Pawn extends Figure {
 
     @Override
     public List<Move> getAvailableMoves(ChessBoard chessBoard, Position position) {
-        return null;
+        List<Move> resultList = new LinkedList<>();
+        if (chessBoard.getAtPosition(position).getFigure().getColor().equals(Color.BLACK)) {
+            if (position.getX() < Constants.endTile) {
+                //posun o jedno dopredu
+                if (chessBoard.getSet()[position.getX() + 1][position.getY()].getFigure() == null) {
+                    resultList.add(new Move(position, chessBoard.getSet()[position.getX() + 1][position.getY()].getPosition()));
+                }
+                //posun o dva dopredu z prvej pozicie
+                if (position.getX() == 3 && chessBoard.getSet()[position.getX() + 1][position.getY()].getFigure() == null && chessBoard.getSet()[position.getX() + 2][position.getY()].getFigure() == null) {
+                    resultList.add(new Move(position, chessBoard.getSet()[position.getX() + 2][position.getY()].getPosition()));
+                }
+                //posun o jedno po diagonale na policko obsadene nepriatelskych hracom
+                if (chessBoard.getSet()[position.getX() + 1][position.getY() - 1].getFigure() != null && chessBoard.getSet()[position.getX() + 1][position.getY() - 1].getFigure().getColor().equals(Color.WHITE)) {
+                    resultList.add(new Move(position, chessBoard.getSet()[position.getX() + 1][position.getY() - 1].getPosition()));
+                }
+                //posun o jedno po diagonale na policko obsadene nepriatelskych hracom
+                if (chessBoard.getSet()[position.getX() + 1][position.getY() + 1].getFigure() != null && chessBoard.getSet()[position.getX() + 1][position.getY() + 1].getFigure().getColor().equals(Color.WHITE)) {
+                    resultList.add(new Move(position, chessBoard.getSet()[position.getX() + 1][position.getY() + 1].getPosition()));
+                }
+            }
+        } else {
+            if (position.getX() > Constants.startTile) {
+                //posun o jedno dopredu
+                if (chessBoard.getSet()[position.getX() - 1][position.getY()].getFigure() == null) {
+                    resultList.add(new Move(position, chessBoard.getSet()[position.getX() - 1][position.getY()].getPosition()));
+                }
+                //posun o dva dopredu z prvej pozicie
+                if (position.getX() == 8 && chessBoard.getSet()[position.getX() - 1][position.getY()].getFigure() == null && chessBoard.getSet()[position.getX() - 2][position.getY()].getFigure() == null) {
+                    resultList.add(new Move(position, chessBoard.getSet()[position.getX() - 2][position.getY()].getPosition()));
+                }
+                //posun o jedno po diagonale na policko obsadene nepriatelskych hracom
+                if (chessBoard.getSet()[position.getX() - 1][position.getY() + 1].getFigure() != null && chessBoard.getSet()[position.getX() - 1][position.getY() + 1].getFigure().getColor().equals(Color.BLACK)) {
+                    resultList.add(new Move(position, chessBoard.getSet()[position.getX() - 1][position.getY() + 1].getPosition()));
+                }
+                //posun o jedno po diagonale na policko obsadene nepriatelskych hracom
+                if (chessBoard.getSet()[position.getX() - 1][position.getY() - 1].getFigure() != null && chessBoard.getSet()[position.getX() - 1][position.getY() - 1].getFigure().getColor().equals(Color.BLACK)) {
+                    resultList.add(new Move(position, chessBoard.getSet()[position.getX() - 1][position.getY() - 1].getPosition()));
+                }
+            }
+        }
+        return resultList;
     }
 
     @Override
